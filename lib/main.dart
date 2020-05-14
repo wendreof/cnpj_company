09b6ea8477e983ec703b2a6ac73477f575991870
WLF <wendreolf@gmail.com>
2020-05-14 02:30:30 -0300

    Refactiong code, adding masks, controllers, textFields and more...

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -28,22 +30,13 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Pesquisa CNPJ'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,92 +45,207 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _formKeyLogin = GlobalKey<FormState>();
+  final _logradouroController = TextEditingController();
+  final _numeroController = TextEditingController();
+  final _cepController = TextEditingController();
+  final _situacaoController = TextEditingController();
+  final _municipioController = TextEditingController();
+  final _fantasiaController = TextEditingController();
+  final _razaoController = TextEditingController();
+  final _complementoController = TextEditingController();
+  final _cnpjController = MaskedTextController(mask: '00.000.000/0000-00');
 
-  void _incrementCounter() async {
-    // setState(() {
-    //   // This call to setState tells the Flutter framework that something has
-    //   // changed in this State, which causes it to rerun the build method below
-    //   // so that the display can reflect the updated values. If we changed
-    //   // _counter without calling setState(), then the build method would not be
-    //   // called again, and so nothing would appear to happen.
-    //   _counter++;
-    // });
+  void _initializeFields() {
+    _logradouroController.text = "";
+    _numeroController.text = "";
+    _cepController.text = "";
+    _situacaoController.text = "";
+    _municipioController.text = "";
+    _fantasiaController.text = "";
+    _cnpjController.text = "";
+  }
+
+  _showMsg(String message) {
+    final snackBar = SnackBar(
+        duration: Duration(seconds: 5),
+        content: Text(
+          message,
+        ));
+    Scaffold.of(_formKeyLogin.currentState.context).showSnackBar(snackBar);
+  }
+
+  String _fixCNPJ(String cnpj) {
+    cnpj = cnpj.replaceAll('.', '');
+    cnpj = cnpj.replaceAll('/', '');
+    cnpj = cnpj.replaceAll('-', '');
+    cnpj = cnpj.replaceAll(',', '');
+    return cnpj;
+  }
+
+  void _getData() async {
     print("************************************************************");
     print("************************ GET_CNPJ *********************");
     print("************************************************************");
-    //print(constants.getSindicate + sindicato.toString());
 
-    // response =
-    //     await http.get(constants.getSindicate + sindicato.toString());
     http.Response response;
-    response =
-        await http.get('http://www.receitaws.com.br/v1/cnpj/26945800000179');
+    var cnpjFixed = _fixCNPJ(_cnpjController.text);
+    print('_cnpjController.text: ${_cnpjController.text}');
+    print('http://www.receitaws.com.br/v1/cnpj/$cnpjFixed');
+    response = await http.get('http://www.receitaws.com.br/v1/cnpj/$cnpjFixed');
 
     print('response.body GET_CNPJ: ${response.body}');
 
     Map<String, dynamic> res = json.decode(response.body);
 
-    var statusCode = res["status"];
+    var statusCode = res['status'];
 
     if (statusCode == 'OK') {
       print('OK, $statusCode');
+
       var logradouro = res["logradouro"];
+      _logradouroController.text = logradouro;
+
+      var numero = res["numero"];
+      _numeroController.text = numero;
+
+      var cep = res["cep"];
+      _cepController.text = cep;
+
+      var situacao = res["situacao"];
+
+      var municipio = res["municipio"];
+      _municipioController.text = municipio;
+
+      var fantasia = res["fantasia"];
+      _fantasiaController.text = fantasia;
+
+      var razao = res["nome"];
+      _razaoController.text = razao;
+
+      var complemento = res["complemento"];
+      _complementoController.text = complemento;
+
       print('logradouro: $logradouro');
+      print('cep: $cep');
+      print('numero: $numero');
+      print('situacao: $situacao');
+      print('municipio: $municipio');
+      print('fantasia: $fantasia');
     } else {
+      _showMsg(res["message"]);
+      _initializeFields();
       print('Error, $statusCode');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    _initializeFields();
+
+    var txtComplemento = TextFormField(
+      maxLines: 2,
+      enabled: false,
+      controller: _complementoController,
+      decoration: InputDecoration(labelText: 'Complemento'),
+    );
+    var txtLogradouro = TextFormField(
+      // key: _formKeyLogin,
+      enabled: false,
+      controller: _logradouroController,
+      decoration: InputDecoration(labelText: 'Logradouro'),
+    );
+    var txtCNPJ = TextField(
+      maxLength: 18,
+      toolbarOptions: ToolbarOptions(
+        cut: true,
+        copy: true,
+        selectAll: true,
+        paste: false,
+      ),
+      enabled: true,
+      textAlign: TextAlign.center,
+      keyboardType: TextInputType.numberWithOptions(),
+      controller: _cnpjController,
+      autofocus: true,
+      cursorColor: Colors.amber,
+      style: TextStyle(height: 1.5, fontSize: 20),
+      cursorWidth: 5.0,
+      //increases the height of cursor
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            borderSide: BorderSide(
+              color: Colors.amber,
+              style: BorderStyle.solid,
+            ),
+          ),
+          labelText: 'CNPJ',
+          hintText: 'Digite um CNPJ'),
+    );
+    var txtMunicipio = TextFormField(
+      enabled: false,
+      controller: _municipioController,
+      decoration: InputDecoration(labelText: 'Munícipio'),
+    );
+    var txtCEP = TextFormField(
+      enabled: false,
+      controller: _cepController,
+      decoration: InputDecoration(labelText: 'CEP'),
+    );
+    var txtNumber = TextFormField(
+      enabled: false,
+      controller: _numeroController,
+      decoration: InputDecoration(labelText: 'Nº'),
+    );
+    var txtNomeFantasia = TextFormField(
+      enabled: false,
+      controller: _fantasiaController,
+      decoration: InputDecoration(labelText: 'Nome Fantasia'),
+    );
+    var txtRazaoSocial = TextFormField(
+      maxLines: 2,
+      enabled: false,
+      controller: _razaoController,
+      decoration: InputDecoration(labelText: 'Razão Social'),
+    );
+    var scaffold = Scaffold(
+      resizeToAvoidBottomInset: false,
+
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        // decoration: BoxDecoration(color: Colors.white),
+        //  padding: EdgeInsets.all(16),
+        child: Form(
+          key: _formKeyLogin,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(12.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  txtCNPJ,
+                  txtLogradouro,
+                  txtMunicipio,
+                  txtCEP,
+                  txtNumber,
+                  txtNomeFantasia,
+                  txtRazaoSocial,
+                  txtComplemento,
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: _getData,
+        tooltip: 'Pesquisar',
+        child: Icon(Icons.search),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+    return scaffold;
   }
 }
