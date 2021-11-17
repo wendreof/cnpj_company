a25fa750fbe2e0418bfd9212c8c607b6683ee750
Wendreo Fernandes <wendreo@outlook.com>
2021-11-16 22:08:14 -0300

    Added: Sentry

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ Future<void> main() async {
   await SentryFlutter.init((options) {
     options.dsn =
         'https://aeccb06d38c4425b98ee1e6daeae73c3@o1069294.ingest.sentry.io/6063716';
+    // ignore: lines_longer_than_80_chars
     // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
     // We recommend adjusting this value in production.
     // options.tracesSampleRate = 1.0;
